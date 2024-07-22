543ad92ed3d7a7465724114542cc636a2e994ca7
Ridwan Sharkar <ridwansharkar@gmail.com>
2024-07-22 17:53:20 -0400

    predict compound activity by CID implemented

--- src/Main.py
+++ src/Main.py
@@ -1,70 +1,138 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import ast  # safe evaluation of string literals to lists
-# import matplotlib.pyplot as plt
+from rdkit import Chem
+from rdkit.Chem import Descriptors, rdMolDescriptors, Crippen
+import joblib  # For loading the model
+import requests
+from rdkit.Chem.rdchem import BondType
 
-# DATA PROCESSING & FEATURE SELECTION===================================================================================
-dataset = 'C:\\Users\\Lenovo\\Desktop\\Psychoactive-Compounds-Analysis\\data\\psychoactiveCompounds_Dataset.csv'
-pharmacologicalActivities = 'C:\\Users\\Lenovo\\Desktop\\Psychoactive-Compounds-Analysis\\data\\compoundInfo_Scraped.csv'
-data = pd.read_csv(dataset)
-activities = pd.read_csv(pharmacologicalActivities)
+# LOAD
+model = joblib.load('C:\\Users\\Lenovo\\Desktop\\Psychoactive-Compounds-Analysis\\model\\RandomForestModel.pkl')
+mlb = joblib.load('C:\\Users\\Lenovo\\Desktop\\Psychoactive-Compounds-Analysis\\model\\MultiLabelBinarizer.pkl')
+# all_descriptors = [desc[0] for desc in Descriptors.descList]
+# print(all_descriptors)
 
-merged_data = pd.merge(data, activities, on='PubChem Compound CID', how='left')
-merged_data['Activities'] = merged_data['Activities'].fillna('[]').apply(ast.literal_eval)
-merged_data = merged_data[merged_data['Activities'].map(len) > 0]
 
-# Transform 'Activities' into binary matrix (Multi-label - Binary Relevance Model with Random Forest ** HAMMING LOSS **
-mlb = MultiLabelBinarizer()
-activities_encoded = mlb.fit_transform(merged_data['Activities'])
+def fetch_smiles(pubchem_cid):
+    """ Fetch the SMILES string from PubChem using its REST API. """
+    url = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{pubchem_cid}/property/CanonicalSMILES/JSON'
+    response = requests.get(url)
+    data = response.json()
+    return data['PropertyTable']['Properties'][0]['CanonicalSMILES']
 
-# FEATURE SELECTION
-features = merged_data[['SlogP', 'SMR', 'LabuteASA', 'TPSA', 'AMW', 'ExactMW', 'NumLipinskiHBA', 'NumLipinskiHBD', 'NumRotatableBonds', 'NumHBD', 'NumHBA', 'NumAmideBonds', 'NumHeteroAtoms', 'NumHeavyAtoms', 'NumAtoms', 'NumRings', 'NumAromaticRings', 'NumSaturatedRings', 'NumAliphaticRings', 'NumAromaticHeterocycles', 'NumSaturatedHeterocycles', 'NumAliphaticHeterocycles', 'NumAromaticCarbocycles', 'NumSaturatedCarbocycles', 'NumAliphaticCarbocycles', 'FractionCSP3', 'Chi0v', 'Chi1v', 'Chi2v', 'Chi3v', 'Chi4v', 'Chi1n', 'Chi2n', 'Chi3n', 'Chi4n', 'HallKierAlpha', 'kappa1', 'kappa2', 'kappa3', 'slogp_VSA1', 'slogp_VSA2', 'slogp_VSA3', 'slogp_VSA4', 'slogp_VSA5', 'slogp_VSA6', 'slogp_VSA7', 'slogp_VSA8', 'slogp_VSA9', 'slogp_VSA10', 'slogp_VSA11', 'slogp_VSA12', 'smr_VSA1', 'smr_VSA2', 'smr_VSA3', 'smr_VSA4', 'smr_VSA5', 'smr_VSA6', 'smr_VSA7', 'smr_VSA8', 'smr_VSA9', 'smr_VSA10', 'peoe_VSA1', 'peoe_VSA2', 'peoe_VSA3', 'peoe_VSA4', 'peoe_VSA5', 'peoe_VSA6', 'peoe_VSA7', 'peoe_VSA8', 'peoe_VSA9', 'peoe_VSA10', 'peoe_VSA11', 'peoe_VSA12', 'peoe_VSA13', 'peoe_VSA14', 'MQN1', 'MQN2', 'MQN3', 'MQN4', 'MQN5', 'MQN6', 'MQN7', 'MQN8', 'MQN9', 'MQN10', 'MQN11', 'MQN12', 'MQN13', 'MQN14', 'MQN15', 'MQN16', 'MQN17', 'MQN18', 'MQN19', 'MQN20', 'MQN21', 'MQN22', 'MQN23', 'MQN24', 'MQN25', 'MQN26', 'MQN27', 'MQN28', 'MQN29', 'MQN30', 'MQN31', 'MQN32', 'MQN33', 'MQN34', 'MQN35', 'MQN36', 'MQN37', 'MQN38', 'MQN39', 'MQN40', 'MQN41', 'MQN42']].fillna(0)
 
-# TRAIN MODEL=============================================================================
-X_train, X_test, y_train, y_test = train_test_split(features, activities_encoded, test_size=0.2, random_state=42)
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
-y_prediction = model.predict(X_test)
+def compute_vsa_descriptors(mol):
+    calculated_descriptors = {}
+    smr_vsa = rdMolDescriptors.SMR_VSA_(mol)
+    for i, value in enumerate(smr_vsa):
+        calculated_descriptors[f'smr_VSA{i + 1}'] = value
 
-# EVALUATE================================================================================
-accuracies = []
-for i, label in enumerate(mlb.classes_):
-    accuracy = accuracy_score(y_test[:, i], y_prediction[:, i])
-    accuracies.append(accuracy)
-    print(f"Accuracy for {label}: {accuracy:.2f}")
+    slogp_vsa = rdMolDescriptors.SlogP_VSA_(mol)
+    for i, value in enumerate(slogp_vsa):
+        calculated_descriptors[f'slogp_VSA{i + 1}'] = value
 
-# AVERAGE ACCURACY========================================================================
-average_accuracy = sum(accuracies) / len(accuracies)
-print(f"Average Accuracy: {average_accuracy:.2f}")
+    peoe_vsa = rdMolDescriptors.PEOE_VSA_(mol)
+    for i, value in enumerate(peoe_vsa):
+        calculated_descriptors[f'peoe_VSA{i + 1}'] = value
 
-# EXAMPLE TEST============================================================================
-test_data = data[data['PubChem Compound CID'] == 44558536]
+    return calculated_descriptors
 
-if not test_data.empty:
-    features = test_data[['SlogP', 'SMR', 'LabuteASA', 'TPSA', 'AMW', 'ExactMW', 'NumLipinskiHBA', 'NumLipinskiHBD', 'NumRotatableBonds', 'NumHBD', 'NumHBA', 'NumAmideBonds', 'NumHeteroAtoms', 'NumHeavyAtoms', 'NumAtoms', 'NumRings', 'NumAromaticRings', 'NumSaturatedRings', 'NumAliphaticRings', 'NumAromaticHeterocycles', 'NumSaturatedHeterocycles', 'NumAliphaticHeterocycles', 'NumAromaticCarbocycles', 'NumSaturatedCarbocycles', 'NumAliphaticCarbocycles', 'FractionCSP3', 'Chi0v', 'Chi1v', 'Chi2v', 'Chi3v', 'Chi4v', 'Chi1n', 'Chi2n', 'Chi3n', 'Chi4n', 'HallKierAlpha', 'kappa1', 'kappa2', 'kappa3', 'slogp_VSA1', 'slogp_VSA2', 'slogp_VSA3', 'slogp_VSA4', 'slogp_VSA5', 'slogp_VSA6', 'slogp_VSA7', 'slogp_VSA8', 'slogp_VSA9', 'slogp_VSA10', 'slogp_VSA11', 'slogp_VSA12', 'smr_VSA1', 'smr_VSA2', 'smr_VSA3', 'smr_VSA4', 'smr_VSA5', 'smr_VSA6', 'smr_VSA7', 'smr_VSA8', 'smr_VSA9', 'smr_VSA10', 'peoe_VSA1', 'peoe_VSA2', 'peoe_VSA3', 'peoe_VSA4', 'peoe_VSA5', 'peoe_VSA6', 'peoe_VSA7', 'peoe_VSA8', 'peoe_VSA9', 'peoe_VSA10', 'peoe_VSA11', 'peoe_VSA12', 'peoe_VSA13', 'peoe_VSA14', 'MQN1', 'MQN2', 'MQN3', 'MQN4', 'MQN5', 'MQN6', 'MQN7', 'MQN8', 'MQN9', 'MQN10', 'MQN11', 'MQN12', 'MQN13', 'MQN14', 'MQN15', 'MQN16', 'MQN17', 'MQN18', 'MQN19', 'MQN20', 'MQN21', 'MQN22', 'MQN23', 'MQN24', 'MQN25', 'MQN26', 'MQN27', 'MQN28', 'MQN29', 'MQN30', 'MQN31', 'MQN32', 'MQN33', 'MQN34', 'MQN35', 'MQN36', 'MQN37', 'MQN38', 'MQN39', 'MQN40', 'MQN41', 'MQN42']].fillna(0)
-    predicted_activities_encoded = model.predict(features)
-    predicted_activities_labels = mlb.inverse_transform(predicted_activities_encoded)
-    print("Predicted Activities for CID 44558536:", predicted_activities_labels)
 
-# FEATURE IMPORTANCES
-feature_names = merged_data[['SlogP', 'SMR', 'LabuteASA', 'TPSA', 'AMW', 'ExactMW', 'NumLipinskiHBA', 'NumLipinskiHBD', 'NumRotatableBonds', 'NumHBD', 'NumHBA', 'NumAmideBonds', 'NumHeteroAtoms', 'NumHeavyAtoms', 'NumAtoms', 'NumRings', 'NumAromaticRings', 'NumSaturatedRings', 'NumAliphaticRings', 'NumAromaticHeterocycles', 'NumSaturatedHeterocycles', 'NumAliphaticHeterocycles', 'NumAromaticCarbocycles', 'NumSaturatedCarbocycles', 'NumAliphaticCarbocycles', 'FractionCSP3', 'Chi0v', 'Chi1v', 'Chi2v', 'Chi3v', 'Chi4v', 'Chi1n', 'Chi2n', 'Chi3n', 'Chi4n', 'HallKierAlpha', 'kappa1', 'kappa2', 'kappa3', 'slogp_VSA1', 'slogp_VSA2', 'slogp_VSA3', 'slogp_VSA4', 'slogp_VSA5', 'slogp_VSA6', 'slogp_VSA7', 'slogp_VSA8', 'slogp_VSA9', 'slogp_VSA10', 'slogp_VSA11', 'slogp_VSA12', 'smr_VSA1', 'smr_VSA2', 'smr_VSA3', 'smr_VSA4', 'smr_VSA5', 'smr_VSA6', 'smr_VSA7', 'smr_VSA8', 'smr_VSA9', 'smr_VSA10', 'peoe_VSA1', 'peoe_VSA2', 'peoe_VSA3', 'peoe_VSA4', 'peoe_VSA5', 'peoe_VSA6', 'peoe_VSA7', 'peoe_VSA8', 'peoe_VSA9', 'peoe_VSA10', 'peoe_VSA11', 'peoe_VSA12', 'peoe_VSA13', 'peoe_VSA14', 'MQN1', 'MQN2', 'MQN3', 'MQN4', 'MQN5', 'MQN6', 'MQN7', 'MQN8', 'MQN9', 'MQN10', 'MQN11', 'MQN12', 'MQN13', 'MQN14', 'MQN15', 'MQN16', 'MQN17', 'MQN18', 'MQN19', 'MQN20', 'MQN21', 'MQN22', 'MQN23', 'MQN24', 'MQN25', 'MQN26', 'MQN27', 'MQN28', 'MQN29', 'MQN30', 'MQN31', 'MQN32', 'MQN33', 'MQN34', 'MQN35', 'MQN36', 'MQN37', 'MQN38', 'MQN39', 'MQN40', 'MQN41', 'MQN42']].columns
-importances = model.feature_importances_
+def compute_mqn_descriptors(mol):
+    mqn_descriptors = rdMolDescriptors.MQNs_(mol)
+    return {f'MQN{i + 1}': val for i, val in enumerate(mqn_descriptors)}
 
-# Create a DataFrame and sort it based on feature importance
-importances_df = pd.DataFrame({'Feature': feature_names, 'Importance': importances})
-importances_df = importances_df.sort_values(by='Importance', ascending=False)
 
-# Print the feature importances in descending order
-print("Feature Importances:")
-for index, row in importances_df.iterrows():
-    print(f"{row['Feature']}: {row['Importance']:.4f}")
+def count_amide_bonds(mol):
+    amide_bond_count = 0
+    for bond in mol.GetBonds():
+        if bond.GetBondType() == BondType.SINGLE:
+            begin_atom = bond.GetBeginAtom()
+            end_atom = bond.GetEndAtom()
+            if (begin_atom.GetAtomicNum() == 6 and end_atom.GetAtomicNum() == 7) or \
+                    (begin_atom.GetAtomicNum() == 7 and end_atom.GetAtomicNum() == 6):
+                if any(n.GetAtomicNum() == 8 and n.GetTotalValence() == 2 for n in begin_atom.GetNeighbors()) or \
+                        any(n.GetAtomicNum() == 8 and n.GetTotalValence() == 2 for n in end_atom.GetNeighbors()):
+                    amide_bond_count += 1
+    return amide_bond_count
 
-# Optional: Print top N features
-top_n = 120
-print("\nTop 120 Important Features:")
-top_features = importances_df.head(top_n)
-for index, row in top_features.iterrows():
-    print(f"{row['Feature']}: {row['Importance']:.4f}")
+# def count_amide_bonds(mol):
+#    """Count the number of amide bonds in a molecule."""
+#    amide_bond_count = 0
+#    for bond in mol.GetBonds():
+#        if bond.GetBondType() == BondType.AMIDE:
+#            amide_bond_count += 1
+#    return amide_bond_count
+
+def compute_descriptors(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return None
+
+    base_descriptors = {
+        'SlogP': Crippen.MolLogP(mol),  # SlogP
+        'SMR': Crippen.MolMR(mol),  # SMR
+        'LabuteASA': rdMolDescriptors.CalcLabuteASA(mol),  # LabuteASA
+        'TPSA': Descriptors.TPSA(mol),  # TPSA
+        'AMW': Descriptors.MolWt(mol),  # AMW
+        'ExactMW': Descriptors.ExactMolWt(mol),  # ExactMW
+        'NumLipinskiHBA': Descriptors.NumHAcceptors(mol),  # NumLipinskiHBA
+        'NumLipinskiHBD': Descriptors.NumHDonors(mol),  # NumLipinskiHBD
+        'NumRotatableBonds': Descriptors.NumRotatableBonds(mol),  # NumRotatableBonds
+        'NumHBD': Descriptors.NumHDonors(mol),  # NumHBD
+        'NumHBA': Descriptors.NumHAcceptors(mol),  # NumHBA
+        'NumAmideBonds': count_amide_bonds(mol),
+        'NumHeteroAtoms': rdMolDescriptors.CalcNumHeteroatoms(mol),
+        'NumHeavyAtoms': Descriptors.HeavyAtomCount(mol),  # NumHeavyAtoms
+        'NumAtoms': mol.GetNumAtoms(),  # NumAtoms
+        'NumRings': rdMolDescriptors.CalcNumRings(mol),  # NumRings
+        'NumAromaticRings': rdMolDescriptors.CalcNumAromaticRings(mol),  # NumAromaticRings
+        'NumSaturatedRings': rdMolDescriptors.CalcNumSaturatedRings(mol),  # NumSaturatedRings
+        'NumAliphaticRings': rdMolDescriptors.CalcNumAliphaticRings(mol),  # NumAliphaticRings
+        'NumAromaticHeterocycles': rdMolDescriptors.CalcNumAromaticHeterocycles(mol),  # NumAromaticHeterocycles
+        'NumSaturatedHeterocycles': rdMolDescriptors.CalcNumSaturatedHeterocycles(mol),  # NumSaturatedHeterocycles
+        'NumAliphaticHeterocycles': rdMolDescriptors.CalcNumAliphaticHeterocycles(mol),  # NumAliphaticHeterocycles
+        'NumAromaticCarbocycles': rdMolDescriptors.CalcNumAromaticCarbocycles(mol),  # NumAromaticCarbocycles
+        'NumSaturatedCarbocycles': rdMolDescriptors.CalcNumSaturatedCarbocycles(mol),  # NumSaturatedCarbocycles
+        'NumAliphaticCarbocycles': rdMolDescriptors.CalcNumAliphaticCarbocycles(mol),  # NumAliphaticCarbocycles
+        'FractionCSP3': rdMolDescriptors.CalcFractionCSP3(mol),  # FractionCSP3
+        'Chi0v': rdMolDescriptors.CalcChi0v(mol),
+        'Chi1v': rdMolDescriptors.CalcChi1v(mol),
+        'Chi2v': rdMolDescriptors.CalcChi2v(mol),
+        'Chi3v': rdMolDescriptors.CalcChi3v(mol),
+        'Chi4v': rdMolDescriptors.CalcChi4v(mol),  # Chi x9
+        'Chi1n': rdMolDescriptors.CalcChi1n(mol),
+        'Chi2n': rdMolDescriptors.CalcChi2n(mol),
+        'Chi3n': rdMolDescriptors.CalcChi3n(mol),
+        'Chi4n': rdMolDescriptors.CalcChi4n(mol),
+        'HallKierAlpha': rdMolDescriptors.CalcHallKierAlpha(mol),  # HallKierAlpha
+        'kappa1': rdMolDescriptors.CalcKappa1(mol),  # kappa1
+        'kappa2': rdMolDescriptors.CalcKappa2(mol),  # kappa2
+        'kappa3': rdMolDescriptors.CalcKappa3(mol),  # kappa3 = 39
+    }
+    base_descriptors.update(compute_vsa_descriptors(mol))
+    base_descriptors.update(compute_mqn_descriptors(mol))
+    return base_descriptors
+
+
+def predict_activities(descriptors):
+    df = pd.DataFrame([descriptors])
+    expected_feature_order = ['SlogP', 'SMR', 'LabuteASA', 'TPSA', 'AMW', 'ExactMW', 'NumLipinskiHBA', 'NumLipinskiHBD', 'NumRotatableBonds', 'NumHBD', 'NumHBA', 'NumAmideBonds', 'NumHeteroAtoms', 'NumHeavyAtoms', 'NumAtoms', 'NumRings', 'NumAromaticRings', 'NumSaturatedRings', 'NumAliphaticRings', 'NumAromaticHeterocycles', 'NumSaturatedHeterocycles', 'NumAliphaticHeterocycles', 'NumAromaticCarbocycles', 'NumSaturatedCarbocycles', 'NumAliphaticCarbocycles', 'FractionCSP3', 'Chi0v', 'Chi1v', 'Chi2v', 'Chi3v', 'Chi4v', 'Chi1n', 'Chi2n', 'Chi3n', 'Chi4n', 'HallKierAlpha', 'kappa1', 'kappa2', 'kappa3', 'slogp_VSA1', 'slogp_VSA2', 'slogp_VSA3', 'slogp_VSA4', 'slogp_VSA5', 'slogp_VSA6', 'slogp_VSA7', 'slogp_VSA8', 'slogp_VSA9', 'slogp_VSA10', 'slogp_VSA11', 'slogp_VSA12', 'smr_VSA1', 'smr_VSA2', 'smr_VSA3', 'smr_VSA4', 'smr_VSA5', 'smr_VSA6', 'smr_VSA7', 'smr_VSA8', 'smr_VSA9', 'smr_VSA10', 'peoe_VSA1', 'peoe_VSA2', 'peoe_VSA3', 'peoe_VSA4', 'peoe_VSA5', 'peoe_VSA6', 'peoe_VSA7', 'peoe_VSA8', 'peoe_VSA9', 'peoe_VSA10', 'peoe_VSA11', 'peoe_VSA12', 'peoe_VSA13', 'peoe_VSA14', 'MQN1', 'MQN2', 'MQN3', 'MQN4', 'MQN5', 'MQN6', 'MQN7', 'MQN8', 'MQN9', 'MQN10', 'MQN11', 'MQN12', 'MQN13', 'MQN14', 'MQN15', 'MQN16', 'MQN17', 'MQN18', 'MQN19', 'MQN20', 'MQN21', 'MQN22', 'MQN23', 'MQN24', 'MQN25', 'MQN26', 'MQN27', 'MQN28', 'MQN29', 'MQN30', 'MQN31', 'MQN32', 'MQN33', 'MQN34', 'MQN35', 'MQN36', 'MQN37', 'MQN38', 'MQN39', 'MQN40', 'MQN41', 'MQN42']
+    prediction_features = df[expected_feature_order]
+    prediction = model.predict(prediction_features)
+    predicted_labels = mlb.inverse_transform(prediction)
+    return predicted_labels
+
+
+def main():
+    cid = input("Enter CID: ")
+    smiles = fetch_smiles(cid)
+    descriptors = compute_descriptors(smiles)
+    if descriptors:
+        activities = predict_activities(descriptors)
+        print(f"Predicted Activities for CID {cid}: {activities}")
+    else:
+        print("Error: Invalid SMILES or CID not found.")
+
+
+if __name__ == "__main__":
+    main()
